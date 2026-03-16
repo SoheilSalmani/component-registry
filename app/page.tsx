271af9b416304343e110d906dea7c12eeaf0125f
Soheil Salmani <salmani.soheil@gmail.com>
2026-03-17 02:56:25 +0700

    Add logomarks from Discord and X

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ import { AppShowcase } from "@/registry/new-york/ui/app-showcase";
 import { GitHubLogomark } from "@/registry/logos/github/github-logomark";
 import { YCombinatorLogomark } from "@/registry/logos/ycombinator/ycombinator-logomark";
 import { ProductHuntLogomark } from "@/registry/logos/product-hunt/product-hunt-logomark";
+import { XLogomark } from "@/registry/logos/x/x-logomark";
+import { DiscordLogomark } from "@/registry/logos/discord/discord-logomark";
 
 export default function Page() {
   return (
@@ -165,6 +167,30 @@ export default function Page() {
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <ProductHuntLogomark />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              The X logomark.
+            </h2>
+            <OpenInV0Button name="example-with-css" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <XLogomark />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              The Discord logomark.
+            </h2>
+            <OpenInV0Button name="example-with-css" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <DiscordLogomark />
           </div>
         </div>
       </main>
