8a388ba58bb28982e63741dec94b106c9a4b5fa9
Soheil Salmani <salmani.soheil@gmail.com>
2026-03-22 19:13:02 +0700

    Add `ThemeToggle` component

---
+++ registry/new-york/ui/theme-toggle.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/registry/new-york/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type ThemeToggleProps = {
+  theme: "light" | "dark";
+} & React.ComponentPropsWithRef<"button">;
+
+function ThemeToggle({ theme, className, ...props }: ThemeToggleProps) {
+  return (
+    <Button
+      size="icon"
+      variant="ghost"
+      className={cn("text-lg cursor-pointer", className)}
+      {...props}
+    >
+      {theme === "light" ? (
+        <SunIcon className="size-5" />
+      ) : (
+        <MoonIcon className="size-5" />
+      )}
+    </Button>
+  );
+}
+
+export { ThemeToggle, type ThemeToggleProps };
